f8134e5c70b93d017e2bc8808cd20ebd617dbc14
shouvikahmed <shouvikahmedantu@gmail.com>
2021-12-22 16:43:46 -0800

    fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import styledComponents from 'styled-components'
+
 import {selectCars} from "../features/car/carSlice";
 import {useSelector} from "react-redux";
 
